c0448a7312317ab956fd067b687b9dd0ed535691
orange-nckname <137512235+orange-nckname@users.noreply.github.com>
2023-10-04 20:29:42 +0800

    Update xlsTOxlsx.py

--- xlsTOxlsx.py
+++ xlsTOxlsx.py
@@ -1,12 +1,31 @@
 import win32com.client as win32
+import os
 
 def reverse_file(fileName):
-    fname = fileName
-    excel = win32.gencache.EnsureDispatch('Excel.Application')
-    wb = excel.Workbooks.Open(fname)
-    wb.SaveAs(fname+"x", FileFormat = 51) #FileFormat = 51 is for .xlsx extension
-    wb.Close() #FileFormat = 56 is for .xls extension
-    excel.Application.Quit()
+    global excel, wb
+    try:
+        fname = fileName
+        fname = os.path.normpath(fname)
+        excel = win32.Dispatch('Excel.Application')
+        # 是否打开excel界面
+        excel.Visible = False
+        # 是否关闭保存弹出框
+        excel.DisplayAlerts = True
+        # 打开当前工作簿
+        wb = excel.Workbooks.Open(fname)
+        # 保存工作簿
+        wb.SaveAs(fname + "x", FileFormat=51)  # FileFormat = 51为xlsx,FileFormat = 56为xls
+        # 关闭工作簿
+        wb.Close()  #
 
-reverse_file("C:\\Users\\thisi\\Desktop\\康复2班 - 副本.xls")
+    except:
+        print("error")
+    finally:
+        # 退出excel,不退出则会被占用
+        # wb.Close()  #
+        excel.Application.Quit()
+        # os.rename(fname, fname + "x")
+        return os.path.normpath(fname + "x")
 
+if __name__ == "__main__":
+    reverse_file("E:/Desktop/a.xls")
